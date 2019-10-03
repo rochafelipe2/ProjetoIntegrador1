bae189634fa6f2ceb6f3ba526565b06c8e512cf6
Igor Vicentini Garcia <38511932+igorvgarcia@users.noreply.github.com>
2019-10-03 20:51:52 -0300

    Criação inicial do Login

---
+++ MercadOn/MercadOn/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using MercadOn.Models.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MercadOn.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        // GET: Login
+        public ActionResult Login()
+        {
+            LoginModel model = new LoginModel();
+            model.Email = "Email";
+            model.Senha = "Senha";
+            return View(model);
+        }
+        
+        [HttpPost]
+        // POST: Login
+        public ActionResult Login(LoginModel model)
+        {
+            return View();
+        }
+    }
+}
