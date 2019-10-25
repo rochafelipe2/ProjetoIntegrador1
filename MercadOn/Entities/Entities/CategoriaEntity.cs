4b4fa0fb569c9f6e8a9a6666d11863f8e3479ccf
Jair <jairjunior02@hotmail.com>
2019-10-24 21:03:58 -0300

    Criacao tabelas BD

---
+++ MercadOn/Entities/Entities/CategoriaEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Entities
+{
+
+
+
+    public class CategoriaEntity
+    {
+        public int idCategoria { get; set; }
+        public string nomeCategoria { get; set; }
+        public int ativo { get; set; }
+    }
+}
---
+++ MercadOn/Entities/Entities/PrecoEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Entities
+{
+    class PrecoEntity
+    {
+        public int idPreco { get; set; }
+        public int idProduto { get; set; }
+        public int idMercado { get; set; }
+        public float precoProduto { get; set; }
+        public ProdutoEntity produtoEntity { get; set; }
+        public MercadoEntity mercadoEntity { get; set; }
+    }
+}
