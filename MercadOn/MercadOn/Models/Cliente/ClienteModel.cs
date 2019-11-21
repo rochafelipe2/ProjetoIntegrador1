f2f9db841c2dbc4c384db58c68c82bff375fac26
Felipe Rocha <lipdiso@gmail.com>
2019-11-21 20:31:31 -0300

    Add cadastro de cliente

---
+++ MercadOn/MercadOn/Models/Cliente/ClienteModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MercadOn.Models.Cliente
+{
+    public class ClienteModel
+    {
+        public long cpf { get; set; }
+        public string nome { get; set; }
+        public int celular { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+
+    }
+}
